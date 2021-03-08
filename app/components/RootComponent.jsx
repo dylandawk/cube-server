d07011529c40a5aedecbb72cd17f1845b9de0eb9
Dylan Dawkins <dad586@nyu.edu>
2021-03-07 22:02:29 -0500

    Adds RootComponent

---
+++ app/components/RootComponent.jsx
@@ -0,0 +1,11 @@
+const React = require("react");
+
+const RootComponent = function () {
+    return (
+        <div>
+            <h1>Hello There!</h1>
+        </div>
+    );
+}
+
+module.exports = RootComponent;
